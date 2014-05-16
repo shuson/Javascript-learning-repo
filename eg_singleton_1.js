88e4c5367c2a165d78125991745b1f4669cfa100
shuson <wlgq@hotmail.com>
2014-05-16 15:34:19 +0800

    Update eg_singleton_1.js

--- eg_singleton_1.js
+++ eg_singleton_1.js
@@ -1,6 +1,18 @@
 var Singleton = {
-    __name:'I am singleton created!' //by my habit properties start with __ are namely private
+    __name:'I am singleton created only once!', //by my habit properties start with __ are namely private
     toString: function(){
         return this.__name; //this is important, since the __name can only be accessed by its own memory owner
     }
 }
+
+
+var s1 = Singleton;
+var s2 = Singleton;
+
+console.log(s1.toString());
+console.log(s2.toString());
+
+---Out put---
+
+I am singleton created only once!
+I am singleton created only once!
