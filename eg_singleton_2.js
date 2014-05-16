25297bc59f5cfc62c19047939eb9a259bd21ee61
shuson <wlgq@hotmail.com>
2014-05-16 18:12:25 +0800

    Update eg_singleton_2.js

--- eg_singleton_2.js
+++ eg_singleton_2.js
@@ -1,4 +1,4 @@
-var Singleton = function(){
+var Singleton = function(){  //this is a constructor for Singleton Object
     if(Singleton.instance==undefined){
         Singleton.instance = this;
     }
@@ -8,3 +8,13 @@ var Singleton = function(){
 Singleton.prototype.toString = function(){
     return "I am created by singleton!";
 }
+
+var s1 = new Singleton(); //using Singleton's constructor to create Singleton Object
+var s2 = new Singleton();
+
+console.log(s1==s2)
+console.log(s2.toString())
+
+-----out put--------
+true
+I am created by singleton!
