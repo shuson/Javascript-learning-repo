6b55e402ffa3507d46908d1ee6971603ddb5dab2
shuson <wlgq@hotmail.com>
2014-06-03 10:36:36 +0800

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,1 +1,18 @@
-function 
+//return the index of target in the sorted array
+function binarSearch(array, target){
+  var low = 0;
+  var high = array.length-1;
+  
+  while(low<=high){
+    var mid = Math.floor((high+low)/2);
+    var value = array[mid];
+    if(value > target){
+      high = mid - 1;
+    }else if(value < target){
+      low = mid + 1;
+    }else{
+      return mid;
+    }
+    return null;
+  }
+}
