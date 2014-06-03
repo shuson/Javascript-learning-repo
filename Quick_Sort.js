4af81f24bb0cd0c024d7caec303219064a0814c2
shuson <wlgq@hotmail.com>
2014-06-03 15:09:37 +0800

    Update Quick_Sort.js

--- Quick_Sort.js
+++ Quick_Sort.js
@@ -1,3 +1,20 @@
 function quickSort(array){
-
+  if(array.length <= 1){
+    return array;
+  }
+  
+  var basisIndex = Math.floor(array.length/2);
+  var basis = array.splice(basisIndex,1)[0]; //cut the basis from the array
+  var subSetLeft = [];
+  var subSetRight = [];
+  
+  for(var i = 0; i < array.length; i++){
+    if(array[i]<basis){
+      subSetLeft.push(array[i]);
+    }else{
+      subSetRight.push(array[i]);
+    }
+  }
+  
+  return quickSort(subSetLeft).concat([basis],quickSort(subSetRight)); //concat the cut basis with left and right subset
 }
