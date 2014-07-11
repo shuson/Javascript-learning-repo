0d7cf55f5f4a9615d4af0c00af510ac28b7e0689
shuson <wlgq@hotmail.com>
2014-07-11 15:38:50 +0800

    Update eg_singleton_1.js

--- eg_singleton_1.js
+++ eg_singleton_1.js
@@ -1,3 +1,4 @@
+'use strict'; // avoid access global object
 function Singleton(){
     if (arguments.callee.instance)
         return arguments.callee.instance;
@@ -9,3 +10,8 @@ Singleton.getInstance = function() {
     var singleton = new Singleton();
     return singleton;
 };
+
+var obj1 = new Singleton();
+var obj2 = new SIngleton();
+
+console.log(obj1 === obj2) // true
